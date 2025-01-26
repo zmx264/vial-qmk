4acc16399d58b9e934f7b43c0e9ab3790ea3ba31
Mihai Stanciu <mihai.stanciu@gmail.com>
2025-01-26 12:16:57 +0200

    my changes

--- keyboards/splitkb/aurora/sofle_v2/lib/bongocat.h
+++ keyboards/splitkb/aurora/sofle_v2/lib/bongocat.h
@@ -1,4 +1,4 @@
-/* Copyright 2023 splitkb.com <support@splitkb.com>
+/* Copyright 2022 HorrorTroll <https://github.com/HorrorTroll>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +16,4 @@
 
 #pragma once
 
-#define OLED_DISPLAY_128X32
-#define SPLIT_OLED_ENABLE
-#define SPLIT_WPM_ENABLE
+void render_bongocat(void);
--- keyboards/splitkb/aurora/sofle_v2/rules.mk
+++ keyboards/splitkb/aurora/sofle_v2/rules.mk
@@ -1,1 +1,4 @@
 DEFAULT_FOLDER = splitkb/aurora/sofle_v2/rev1
+CONVERT_TO=liatris
+VIAL_INSECURE = yes
+SRC += lib/bongocat.c
